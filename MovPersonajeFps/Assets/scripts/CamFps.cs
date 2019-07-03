a36e6254facfdc08a8df8e969e4161ef061613ac
quiru <reacosta07@gmail.com>
2019-07-03 17:02:13 -0500

    commenting the code
    all code coment

--- MovPersonajeFps/Assets/scripts/CamFps.cs
+++ MovPersonajeFps/Assets/scripts/CamFps.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CamFps : MonoBehaviour
 {
-    float mouseX;
-    float mouseY;
+    float mouseX; // variable para guardar valores del movimiento del mouse en X 
+    float mouseY; // variable para guardar valores del movimiento del mouse en Y
 
     void Update()
     {
-        mouseX += Input.GetAxis("Mouse X");
-        mouseY -= Input.GetAxis("Mouse Y");
+        mouseX += Input.GetAxis("Mouse X"); // captura el movimiento del mouse y se lo suma a mouseX para el movimiento de la camara en x
+        mouseY -= Input.GetAxis("Mouse Y"); // captura el movimiento del mouse y se lo resta a mouseY para el movimiento de la camara en y
 
-        if (mouseY > 40)
+        if (mouseY > 40) // condicion para que no mire mas de 40 hacia arriba y menos de 40 hacia abajo (para que no gire la camara en y)
         {
             mouseY = 40;
         }
@@ -20,7 +20,6 @@ public class CamFps : MonoBehaviour
         {
             mouseY = -40;
         }
-        Debug.Log(mouseY);
-        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0); //mueve la camara segun el vaor que alla en
     }
 }
--- MovPersonajeFps/Assets/scripts/MovFps.cs
+++ MovPersonajeFps/Assets/scripts/MovFps.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class MovFps : MonoBehaviour
 {
-    public float speed = 0.1f;
-    float mouseX;
+    public float speed = 0.1f; // para darle una velocidad al movimiento
+    float mouseX; // para capturar la entrada del mouse
 
     void Update()
     {
-        mouseX += Input.GetAxis("Mouse X");
-        transform.eulerAngles = new Vector3(0, mouseX, 0);
+        mouseX += Input.GetAxis("Mouse X"); // va agregando numeros segun el movimiento del mouse en x
+        transform.eulerAngles = new Vector3(0, mouseX, 0); // mueve la camara segun el vaor que alla en mouseX
 
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)) //si se presiona la tecla w
         {
-            transform.position += transform.forward * speed;
+            transform.position += transform.forward * speed; //se mueve hacia adelante
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)) //si se presiona la tecla s
         {
-            transform.position -= transform.forward * speed;
+            transform.position -= transform.forward * speed; //se mueve hacia atras
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)) // si se presiona la tecla a
         {
-            transform.position -= transform.right * speed;
+            transform.position -= transform.right * speed; // se mueve a la izquierda
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)) // si se presiona la tecla d
         {
-            transform.position += transform.right * speed;
+            transform.position += transform.right * speed; // se mueve hacia la derecha
         }
     }
 }
